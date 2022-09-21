df3735e64de36d5ecf635c81dcba25fe2a95f989
Jiheng Nie <Niejiheng@gmail.com>
2022-09-21 19:08:45 +0000

    Adjusted inline styling to classes

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var $carHolder = document.querySelector('.car-holder');
 window.addEventListener('keydown', changingDirection);
 
 function changingDirection(event) {
-  if (event.key === 'ArrowLeft') {
-    $carHolder.style.transform = 'rotate(180deg)';
-  } else if (event.key === 'ArrowUp') {
-    $carHolder.style.transform = 'rotate(-90deg)';
+  if (event.key === 'ArrowUp') {
+    $carHolder.className = 'car-holder up';
+  } else if (event.key === 'ArrowLeft') {
+    $carHolder.className = 'car-holder left';
   } else if (event.key === 'ArrowDown') {
-    $carHolder.style.transform = 'rotate(90deg)';
+    $carHolder.className = 'car-holder down';
   } else if (event.key === 'ArrowRight') {
-    $carHolder.style.transform = 'rotate(0deg)';
+    $carHolder.className = 'car-holder right';
   }
 
 }
