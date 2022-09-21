9b9177f71eee8449645273910f701b392022f0e3
Jiheng Nie <Niejiheng@gmail.com>
2022-09-21 20:32:55 +0000

    Added in ability to move stright for a while

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 var $carHolder = document.querySelector('.car-holder');
+var $car = document.querySelector('.car');
+var carLocation = {
+  x: 0,
+  y: 0
+};
+var intervalID = setInterval(movingStright, 16);
 
 window.addEventListener('keydown', changingDirection);
 
 function changingDirection(event) {
   if (event.key === 'ArrowUp') {
-    $carHolder.className = 'car-holder up';
+    $car.className = 'car up';
   } else if (event.key === 'ArrowLeft') {
-    $carHolder.className = 'car-holder left';
+    $car.className = 'car left';
   } else if (event.key === 'ArrowDown') {
-    $carHolder.className = 'car-holder down';
+    $car.className = 'car down';
   } else if (event.key === 'ArrowRight') {
-    $carHolder.className = 'car-holder right';
+    $car.className = 'car right';
   }
 
+}
+
+function movingStright() {
+  $carHolder.style.transform = 'translate(' + carLocation.x + 'px,' + carLocation.y + 'px)';
+  carLocation.x = carLocation.x + 2;
+  if (carLocation.x === 1000) {
+    clearInterval(intervalID);
+  }
 }
